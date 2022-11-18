d9b51dc55fc46675888571fbd07483b5bbe38a0e
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-11-17 21:08:38 -0600

    Started assignment. Added repository interfaces for Employer and Skill. Fleshed out AbstractEntity. Completed EmployerController methods.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 2000)
+    private String description;
+
+    public Skill(@Size(max = 2000) String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }

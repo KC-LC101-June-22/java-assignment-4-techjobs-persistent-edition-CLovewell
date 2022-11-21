92b46d99085564ae238607e7fee37fbea20e111a
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-11-21 11:08:22 -0600

    ArrayList of Jobs added to Employer.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -3,6 +3,8 @@ package org.launchcode.techjobs.persistent.models;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +12,8 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "A location is required")
     @Size(max = 60, message = "That location name is too long")
     private String location;
+
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(@NotBlank @Size(max = 60) String location) {
         this.location = location;
